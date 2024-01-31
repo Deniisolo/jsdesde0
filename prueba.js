8402b060f25fc0cb7c15670ecaea453f0506ceb5
Deniisolo <deniisolo@outlook.es>
2024-01-31 06:31:30 -0500

    ejercicio de AND

--- prueba.js
+++ prueba.js
@@ -6,3 +6,11 @@ function incrementoConometro(){
 console.log(contador)
 incrementoConometro()
 console.log(contador)
+
+// OPERDORES lOGICOS 
+
+let condicion1 = true;
+let condicion2 = true;
+
+let resultadoAnd = condicion1 && condicion2;
+console.log(resultadoAnd); 
