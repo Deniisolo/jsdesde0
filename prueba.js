8a48baa08f1bfa0bb2824273df9de97ca616cca3
Deniisolo <deniisolo@outlook.es>
2024-02-06 15:23:59 -0500

    ejercicio de For

--- prueba.js
+++ prueba.js
@@ -1,52 +1,57 @@
-let contador = 1;
+// let contador = 1;
 
-function incrementoConometro(){
-    contador++;
+// function incrementoConometro(){
+//     contador++;
+// }
+// console.log(contador)
+// incrementoConometro()
+// console.log(contador)
+
+// // OPERDORES lOGICOS 
+
+// let condicion1 = true;
+// let condicion2 = true;
+
+// let resultadoAnd = condicion1 && condicion2;
+// console.log(resultadoAnd); 
+
+// // Coerción
+// let number = 10;
+// let texto = "10";
+// let resultado = number + texto;
+// console.log(resultado)
+
+
+// function calculateTip (billAmount, tipPercentage){
+//     console.log(billAmount * (tipPercentage / 100))
+// }
+// calculateTip(300, 10)
+
+// let diaSemana = 5;
+// let mensaje;
+// switch (diaSemana) {
+//     case 1: 
+//     mensaje = "Lunes";
+//     break;
+//     case 2:
+//         mensaje = "Martes";
+//         break;
+//     case 3:
+//         mensaje = "Miércoles";
+//         break;
+//     case 4:
+//         mensaje = "Jueves";
+//         break;
+//     case 5:
+//         mensaje = "Viernes";
+//         break;
+//     default:
+//         mensaje = "Fin de semana";
+// }
+
+// console.log("Hoy es " + mensaje);
+
+for (let i = 0; i < 10; i++) {
+    console.log("El valor de i es:", i);
 }
-console.log(contador)
-incrementoConometro()
-console.log(contador)
 
-// OPERDORES lOGICOS 
-
-let condicion1 = true;
-let condicion2 = true;
-
-let resultadoAnd = condicion1 && condicion2;
-console.log(resultadoAnd); 
-
-// Coerción
-let number = 10;
-let texto = "10";
-let resultado = number + texto;
-console.log(resultado)
-
-
-function calculateTip (billAmount, tipPercentage){
-    console.log(billAmount * (tipPercentage / 100))
-}
-calculateTip(300, 10)
-
-let diaSemana = 5;
-let mensaje;
-switch (diaSemana) {
-    case 1: 
-    mensaje = "Lunes";
-    break;
-    case 2:
-        mensaje = "Martes";
-        break;
-    case 3:
-        mensaje = "Miércoles";
-        break;
-    case 4:
-        mensaje = "Jueves";
-        break;
-    case 5:
-        mensaje = "Viernes";
-        break;
-    default:
-        mensaje = "Fin de semana";
-}
-
-console.log("Hoy es " + mensaje);
