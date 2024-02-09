30f096992c3c8fdc2bb58cf9a2fc19c84e796cbf
Deniisolo <deniisolo@outlook.es>
2024-02-09 18:20:10 -0500

    ejercicio de js

--- prueba.js
+++ prueba.js
@@ -112,3 +112,20 @@
 // }else {
 //     console.log("tas locooo")
 // }
+function encontrarMayor(numero1, numero2, numero3)
+{
+    if (numero1 > numero2 && numero1 > numero3) {
+        return numero1;
+    } else if (numero2 > numero1 && numero2 > numero3) {
+        return numero2;
+    } else if (numero3 > numero1 && numero3 > numero2) {
+        return numero3;
+    } else {
+        return "tas locooo"; 
+}
+}
+let numero1 = 1;
+let numero2 = 3;
+let numero3 = 2;
+let mayor = encontrarMayor(numero1, numero2, numero3);
+console.log("El número mayor es:", mayor);
